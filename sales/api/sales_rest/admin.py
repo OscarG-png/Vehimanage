fbe5f4f511c7b8763863de5725be39d62a338117
OscarG <oscar.gamez88@gmail.com>
2023-10-23 15:22:37 -0700

    Created models for Salesperson, Customer, Sale, and AutomobileVO. Also started on the Poller, Currently need to debugg why it's not successfully polling

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Salesperson, Customer, AutomobileVO, Sale
+
 
 # Register your models here.
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    list_display = [
+        "vin",
+        "sold",
+    ]
+
+
+@admin.register(Salesperson)
+class SalespersonAdmin(admin.ModelAdmin):
+    list_display = [
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        "first_name",
+        "last_name",
+        "address",
+        "phone_number",
+    ]
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = [
+        "salesperson",
+        "customer",
+    ]
--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+
+
+class Sale(models.Model):
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="sale",
+        on_delete=models.PROTECT
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sale",
+        on_delete=models.PROTECT
+    )
+
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=50, unique=True)
+    sold = models.BooleanField(default=False)
