b86b9aa8e07fc801a071e9e20d75ad0566cf8882
OscarG <oscar.gamez88@gmail.com>
2023-10-23 21:11:18 -0700

    Created the form for creating a salesperson, but it's not done. Need to handle the states and be able to send a post request to the database

--- ghi/app/src/SalesPersonCreate.js
+++ ghi/app/src/SalesPersonCreate.js
@@ -3,13 +3,17 @@ import React from "react";
 function SalesPeopleCreate() {
     return (
         <div className="my-3 container">
-            <div class="mb-3">
-                <label for="formGroupExampleInput" class="form-label">Example label</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder"/>
+            <div className="mb-3">
+                <label htmlFor="formGroupExampleInput" className="form-label">First name</label>
+                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="First name"/>
             </div>
-            <div class="mb-3">
-                <label for="formGroupExampleInput2" class="form-label">Another label</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder"/>
+            <div className="mb-3">
+                <label htmlFor="formGroupExampleInput2" className="form-label">Last Name</label>
+                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Last name"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="formGroupExampleInput2" className="form-label">Employee ID</label>
+                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Employee ID"/>
             </div>
         </div>
     )
