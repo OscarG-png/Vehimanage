a101a1bc7c3a01e4caca4859370fc35d5a7aa71c
OscarG <oscar.gamez88@gmail.com>
2023-10-25 16:18:51 -0700

    forgot to add $ before price for the salesperson history component

--- ghi/app/src/SalesPeronHistory.js
+++ ghi/app/src/SalesPeronHistory.js
@@ -63,7 +63,7 @@ function SalesPersonHistory() {
                                 <td>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
                                 <td>{sale.customer.first_name} {sale.customer.last_name}</td>
                                 <td>{sale.automobile.vin}</td>
-                                <td>{sale.price}</td>
+                                <td>${sale.price}</td>
                             </tr>
                         )
                     })}
