aed759c5a9d88b64a6ba0d18157e604ac7b50211
OscarG <oscar.gamez88@gmail.com>
2023-10-25 17:20:53 -0700

    finished the automobile list componenet

--- ghi/app/src/AutomobileList.js
+++ ghi/app/src/AutomobileList.js
@@ -1,16 +1,46 @@
+import React, { useEffect, useState} from "react";
+
 function AutomobileList() {
+    const [automobiles, setAutomobiles] = useState([]);
+    const fetchData= async () => {
+        const url = "http://localhost:8100/api/automobiles/";
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            setAutomobiles(data.autos)
+            console.log(data.autos)
+        }
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
     return(
         <div>
         <h1>Automobiles</h1>
         <table className="table table-striped">
             <thead>
                 <tr>
-                    <th>Employee id</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
+                    <th>VIN</th>
+                    <th>Color</th>
+                    <th>Year</th>
+                    <th>Model</th>
+                    <th>Manufacturer</th>
+                    <th>Sold</th>
                 </tr>
             </thead>
             <tbody>
+                {automobiles.map(auto => {
+                    return (
+                        <tr key={auto.id}>
+                            <td>{auto.vin}</td>
+                            <td>{auto.color}</td>
+                            <td>{auto.year}</td>
+                            <td>{auto.model.name}</td>
+                            <td>{auto.model.manufacturer.name}</td>
+                            <td>{auto.sold ? "Yes" : "No"}</td>
+                        </tr>
+                    )
+                })}
             </tbody>
     </table>
     </div>
